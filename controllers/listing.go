dc08226fbcf52ff2a08a85ee72cac99a30d0d5ca
daniel <aciddaniel@gmail.com>
2018-07-08 15:24:23 +0200

    getting address name from query params

--- controllers/listing.go
+++ controllers/listing.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 
 	models "../models"
@@ -45,28 +46,64 @@ func getJSON(url string, target interface{}) error {
 }
 
 func getListingsURL(location string) string {
-	return fmt.Sprintf("https://www.airbnb.com/api/v2/explore_tabs?version=1.3.2&_format=for_explore_search_web&experiences_per_grid=20&items_per_grid=50&guidebooks_per_grid=0&auto_ib=true&fetch_filters=true&is_guided_search=false&is_new_trips_cards_experiment=true&is_new_homes_cards_experiment=false&luxury_pre_launch=false&screen_size=large&show_groupings=false&supports_for_you_v3=true&timezone_offset=120&metadata_only=false&is_standard_search=true&selected_tab_id=all_tab&tab_id=home_tab&location=%v&federated_search_session_id=e30fad3d-4dfd-4348-b72a-bb2d1f53ca0c&_intents=p1&screen_size=large&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&currency=USD&locale=en", location)
+	return fmt.Sprintf("https://www.airbnb.com/api/v2/explore_tabs"+
+		"?version=1.3.2"+
+		"&_format=for_explore_search_web"+
+		"&experiences_per_grid=20"+
+		"&items_per_grid=50"+
+		"&guidebooks_per_grid=0"+
+		"&auto_ib=true"+
+		"&fetch_filters=true"+
+		"&is_guided_search=false"+
+		"&is_new_trips_cards_experiment=true"+
+		"&is_new_homes_cards_experiment=false"+
+		"&luxury_pre_launch=false"+
+		"&screen_size=large"+
+		"&show_groupings=false"+
+		"&supports_for_you_v3=true"+
+		"&timezone_offset=120"+
+		"&metadata_only=false"+
+		"&is_standard_search=true"+
+		"&selected_tab_id=all_tab"+
+		"&tab_id=home_tab"+
+		"&location=%v"+
+		"&federated_search_session_id=e30fad3d-4dfd-4348-b72a-bb2d1f53ca0c"+
+		"&_intents=p1"+
+		"&screen_size=large"+
+		"&key=d306zoyjsyarp7ifhu67rjxn52tv0t20"+
+		"&currency=USD"+
+		"&locale=en", location)
 }
 
 // Listing root path handler
-func Listing(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func Listing(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	listingsMetadata := models.ListingsMetadata{}
-	url := getListingsURL("london")
+	queryValues := r.URL.Query()
+	street := queryValues.Get("street")
 
-	err := getJSON(url, &listingsMetadata)
-	if err != nil {
-		fmt.Println(err)
+	if street != "" {
+		sn := &url.URL{Path: street}
+		esn := sn.String()
+
+		url := getListingsURL(esn)
+
+		err := getJSON(url, &listingsMetadata)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, http.StatusText(403), http.StatusForbidden)
+			return
+		}
+
+		json, errMarshal := json.Marshal(listingsMetadata)
+		if errMarshal != nil {
+			fmt.Println(err)
+			http.Error(w, http.StatusText(403), http.StatusForbidden)
+			return
+		}
+
+		w.Write(json)
+	} else {
 		http.Error(w, http.StatusText(403), http.StatusForbidden)
-		return
 	}
-
-	json, errMarshal := json.Marshal(listingsMetadata)
-	if errMarshal != nil {
-		fmt.Println(err)
-		http.Error(w, http.StatusText(403), http.StatusForbidden)
-		return
-	}
-
-	w.Write(json)
 }
